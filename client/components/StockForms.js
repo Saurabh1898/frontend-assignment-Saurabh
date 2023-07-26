8b30611f1adf13fe90353cff618ab295e6bda9be
Saurabh1898 <69433854+Saurabh1898@users.noreply.github.com>
2023-07-26 18:43:03 +0530

    Update StockForms.js

--- client/components/StockForms.js
+++ client/components/StockForms.js
@@ -1,1 +1,29 @@
+// StockForm.js
+import React, { useState } from 'react';
+
+const StockForm = ({ onSubmit }) => {
+  const [symbol, setSymbol] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ symbol, date });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Stock Symbol:
+        <input type="text" value={symbol} onChange={(e) => setSymbol(e.target.value)} required />
+      </label>
+      <label>
+        Date:
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default StockForm;
 
